75f8bc239f471bc7b08b617ef02b0a9529966bfe
Luciana <laf.0182@gmail.com>
2023-03-11 01:39:04 -0300

    first commit

---
+++ src/main/java/com/codoacodo/tamagotchiproject/TamagotchiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codoacodo.tamagotchiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagotchiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamagotchiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codoacodo/tamagotchiproject/TamagotchiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codoacodo.tamagotchiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamagotchiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
