857989b01b9fe8d77469a2fbbbc30d0defe1bd33
Luciana <laf.0182@gmail.com>
2023-03-11 18:52:52 -0300

    Add attributes, methods and main

--- src/main/java/com/codoacodo/tamagotchiproject/tamagotchi/Principal.java
+++ src/main/java/com/codoacodo/tamagotchiproject/tamagotchi/Principal.java
@@ -1,4 +1,48 @@
 package com.codoacodo.tamagotchiproject.tamagotchi;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Para crear su Tamagotchi, coloquele un nombre:");
+        String name = scanner.nextLine();
+
+        System.out.println("Coloque un estado a su mascota virtual:");
+        String state = scanner.nextLine();
+
+        //Creo Tamagotchi
+        Tamagotchi tama = new Tamagotchi(name, state);
+
+        //Consulto estado
+        System.out.println(name + " está: " + tama.getEstado());
+
+        //Le doy de comer
+        tama.comer();
+
+        //Consulto estado nuevamente
+        System.out.println("Luego de comer, " + name + " está: " + tama.getEstado() + " y su nivel es: " +
+                tama.getNivel());
+
+        /*--------------------------------------------------------------------------------------------------------*/
+        System.out.println("------------------------------------------------------------");
+
+        //Consulto estado
+        System.out.println(name + " está: " + tama.getEstado());
+
+        //La hago jugar
+        tama.jugar();
+
+        //Consulto estado nuevamente
+        System.out.println("Luego de jugar, " + name + " está: " + tama.getEstado() + " y su nivel es: " +
+                tama.getNivel());
+
+        /*Tamagotchi tama = new Tamagotchi();
+        tama.setNombre("Leia");
+        tama.setEstado("hambrienta");
+        */
+
+    }
 }
--- src/main/java/com/codoacodo/tamagotchiproject/tamagotchi/Tamagotchi.java
+++ src/main/java/com/codoacodo/tamagotchiproject/tamagotchi/Tamagotchi.java
@@ -2,4 +2,101 @@ package com.codoacodo.tamagotchiproject.tamagotchi;
 
 public class Tamagotchi {
 
+    private String nombre;
+    private Integer nivel;
+    private String estado;
+    private Integer tiempoEnMinutos;
+
+
+    //Constructor sin parámetros pero con valores por defecto al crearse:
+    public Tamagotchi(){
+        this.nivel = 0;
+        this.tiempoEnMinutos = 0;
+    }
+
+    //Constructor con parámetros solo si le ingresan un nombre y un estado + valores por defecto:
+    public Tamagotchi(String nombre, String estado) {
+        this.nombre = nombre;
+        this.nivel = 0;
+        this.estado = estado;
+        this.tiempoEnMinutos = 0;
+    }
+
+    //Métodos:
+    public void comer(){
+        switch (this.estado){
+            case "hambrienta":
+                this.estado = "contenta";
+                break;
+            case "contenta":
+                this.nivel++;
+                break;
+            case "aburrida":
+                if (this.tiempoEnMinutos > 80){
+                    this.estado = "contenta";
+                }
+                break;
+            default:
+                System.out.println("Estado desconocido");
+                break;
+        }
+    }
+
+    public void jugar(){
+        if (this.puedeJugar()){
+            switch (this.estado){
+                case "contenta":
+                    this.nivel += 2;
+                    break;
+                case "aburrida":
+                    this.estado = "contenta";
+                    break;
+                /*case "hambrienta":
+                    break;*/
+                default:
+                    System.out.println("Estado desconocido");
+                    break;
+            }
+        }
+    }
+
+    public Boolean puedeJugar(){
+        return this.estado.equals("hambrienta") ? false : true;
+    }
+
+    //Getters & Setters:
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Integer nivel) {
+        if(nivel < 0){
+            this.nivel = 0;
+        }
+        this.nivel = nivel;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Integer getTiempoEnMinutos() {
+        return tiempoEnMinutos;
+    }
+
+    public void setTiempoEnMinutos(Integer tiempoEnMinutos) {
+        this.tiempoEnMinutos = tiempoEnMinutos;
+    }
 }
