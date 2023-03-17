d1e8ea70231d67a633d8398e8bb7e357d02c7f93
Luciana <laf.0182@gmail.com>
2023-03-17 04:35:13 -0300

    Add comments (in review)

--- src/test/java/com/codoacodo/tamagotchiproject/tamagotchi/TamagotchiTest.java
+++ src/test/java/com/codoacodo/tamagotchiproject/tamagotchi/TamagotchiTest.java
@@ -5,15 +5,28 @@ import org.junit.Test;
 
 public class TamagotchiTest {
 
+    //Tamagotchi tama; //declaro el Objeto
+
+    /*void setUp(){
+        //Tamagotchi tama = new Tamagotchi(); //este Objeto "tama" lo vamos a poder utilizar en c/u de los tests
+        //es decir, ese Objeto va a vivir en c/u de los métodos @Test que creemos.
+        //Acá El setUp ya me va a crear el Tamagotchi con el constructor vacío por defecto (sin nombre)
+
+        //Instancio el Objeto
+        tama = new Tamagotchi(); //este Objeto "tama" lo voy a utilizar en testQuePruebaQueAlComerConEstadoHambrientaCambiaElEstadoAContenta()
+    }
+    */
+
+
     //1) Creo el Test con un nombre que explique lo que se va a probar:
     @Test
     public void testQuePruebaQuePuedoCrearUnTamagotchiConNombre(){
         //Creo el Objeto y ejecuto la prueba brindandole un nombre como indica el test:
-        Tamagotchi mascota = new Tamagotchi("Tama"); //esta funcionalidad no está en el constructor de la clase real. Lo creo.
+        Tamagotchi mascota1 = new Tamagotchi("Tama"); //esta funcionalidad no está en el constructor de la clase real. Lo creo.
                                                    // El nombre en el constructor es lo mismo que hacer --> mascota.setNombre("Tama");
 
         //Utilizo la clase Assert para validar si estamos obteniendo el resultado esperado:
-        Assert.assertEquals(mascota.getNombre(), "Tama"); //compara si el nombre ingresado por parametro arriba (el esperado) es igual al nombre actual
+        Assert.assertEquals(mascota1.getNombre(), "Tama"); //compara si el nombre ingresado por parametro arriba (el esperado) es igual al nombre actual
                              //Object expected - object actual
     }
 
@@ -21,8 +34,12 @@ public class TamagotchiTest {
     public void testQuePruebaQueAlComerConEstadoHambrientaCambiaElEstadoAContenta(){
         Tamagotchi mascota = new Tamagotchi();
         mascota.setEstado("hambrienta");
-        mascota.comer();
+        mascota.comer(); //ejecuto la funcionalidad que quiero probar
         Assert.assertEquals(mascota.getEstado(),"contenta");
+        //Hacemos que el estado actual de la mascota coincida con el estado obtenido
+
+        //Esperado:
+        //Actual: contenta
     }
 
     @Test
